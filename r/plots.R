474ef03e588963756ad4eaa24c2746f9e288d109
Niko <nikolas.hoeft@yahoo.de>
2018-01-13 14:18:15 +0100

    updated heatmap function

--- r/plots.R
+++ r/plots.R
@@ -3,33 +3,32 @@ library(dplyr)
 library(reshape2)
 
 
-m = as.matrix(matrix(rnorm(9), ncol = 3))
-
-
-
-
-heatmap_custom <- function(C){
+heatmap_custom = function(C, param_names = NULL, title = NULL, legend_title = NULL){
     
     C = as.matrix(C)
-    rownames(C) = colnames(C) = paste("X", 1:nrow(C))
+    
+    if(is.null(param_names)){
+        rownames(C) = colnames(C) = paste("X", 1:nrow(C))
+    } else {
+        rownames(C) = colnames(C) = param_names
+    }
+    
     C_melted <- melt(C)
     
     
-    g1 <- ggplot(data = C_melted, aes(x=Var1, y=Var2, fill=value)) + 
+    g1  = ggplot(data = C_melted, aes(x=Var1, y=Var2, fill=value)) + 
         geom_tile() + 
-        scale_fill_continuous(low = "white", high = "black", name = "") + 
-        #guides(fill=guide_legend(title="Correlation")) +
-        theme(axis.title.x=element_blank(),
-              axis.title.y=element_blank()) + 
-        ggtitle("Some Title") + 
+        scale_fill_continuous(low = "#fef0d9", high = "#a50f15", name = "") + 
+        #guides(fill=guide_legend(title=legend_title)) +
+        ggtitle(title) + 
         theme_bw()  + 
         theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-              legend.key = element_rect(colour = "black"), plot.title = element_text(face="bold"),
-              panel.border = element_rect(colour = "black", fill=NA, size=1)) + 
-        theme(axis.text.y = element_text(size = 12, hjust = 0))
-    
-    
+              legend.key = element_rect(colour = "black"), plot.title = element_text(face="bold", hjust = .5),
+              panel.border = element_rect(colour = "black", fill=NA, size=.3), 
+              axis.title.x=element_blank(),
+              axis.title.y=element_blank()) 
+        
     return(g1)
 }
 
-heatmap_custom(m)
+heatmap_custom(m, c("Hallo", "Welt", "Test"), title = "Plot Title")
