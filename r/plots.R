bdb7e99005419679fc97bbb5c67143509dd367c3
Niko <nikolas.hoeft@yahoo.de>
2018-01-16 15:04:38 +0100

    .

--- r/plots.R
+++ r/plots.R
@@ -22,13 +22,19 @@ heatmap_custom = function(C, param_names = NULL, title = NULL, legend_title = NU
         #guides(fill=guide_legend(title=legend_title)) +
         ggtitle(title) + 
         theme_bw()  + 
-        theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-              legend.key = element_rect(colour = "black"), plot.title = element_text(face="bold", hjust = .5),
+        theme(panel.grid.major = element_blank(), 
+              panel.grid.minor = element_blank(),
+              legend.key = element_rect(colour = "black"), 
+              plot.title = element_text(face="bold", hjust = .5, size = 15),
               panel.border = element_rect(colour = "black", fill=NA, size=.3), 
               axis.title.x=element_blank(),
-              axis.title.y=element_blank()) 
+              axis.title.y=element_blank(),
+              axis.text.x = element_text(size = 15, face = "bold"),
+              axis.text.y = element_text(size = 15, face = "bold")) 
         
     return(g1)
 }
 
-heatmap_custom(m, c("Hallo", "Welt", "Test"), title = "Plot Title")
+m = matrix(rnorm(9), ncol = 3)
+
+heatmap_custom(m, c("Mu", "Sigma11", "Sigma12"), title = "Plot Title")
