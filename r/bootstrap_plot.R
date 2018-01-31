fd0f10eb2cb16310f6c4f7636dd0083a1ee27ea0
schreckf <freddi.s@gmx.net>
2018-01-31 20:25:13 +0100

    plot updating

--- r/bootstrap_plot.R
+++ r/bootstrap_plot.R
@@ -1,13 +1,15 @@
-par_var_boot = c(12.42484, 36.80615, 50.73317, 56.05955, 0.7227770)
-par_var_impute = c(12.42484, 36.80615, 50.73317, 56.05955, 0.7227770) * 0.7
-par_var_real = c(12.42484, 36.80615, 50.73317, 56.05955, 0.7227770) * 0.97
+bootstrapped = c(12.42484, 36.80615, 50.73317, 56.05955, 0.7227770)
+imputed = c(12.42484, 36.80615, 50.73317, 56.05955, 0.7227770) * 0.7
+true = c(12.42484, 36.80615, 50.73317, 56.05955, 0.7227770) * 0.97
 
 Names=c("mu 1","sigma 1", "mu 2", "sigma 2", "rho" ) 
-df = data.frame(cbind(par_var_boot, par_var_impute, par_var_real), Names)
+df = data.frame(cbind(bootstrapped, imputed, true), Names)
 
 # melt the data frame for plotting
 df_m <- melt(df, id.vars='Names')
 
 # plot everything
 ggplot(df_m, aes(Names, value)) +   
-    geom_bar(aes(fill = variable), position = "dodge", stat="identity")
+    geom_point(aes(color = variable, shape = variable), position = "dodge", stat="identity", size = 5) +
+    labs(x = "parameters", y = "variance") +
+    guides(fill=guide_legend(title="type"), position_dodge(width = 2)) 
